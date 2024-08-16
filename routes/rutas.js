29f26670fe758ba3564f1b8b6d547147f6be7d0d
jromo1506 <jromo1506mx@gmail.com>
2024-08-16 15:02:10 -0600

    Issue at userAuth

--- routes/rutas.js
+++ routes/rutas.js
@@ -5,7 +5,8 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.post('/addUser',userController.addUser);
-router.get('/authUser',userController.authUser);
+router.post('/authUser',userController.authUser);
+router.post('/getUser',userController.getUserById);
 
 
 
